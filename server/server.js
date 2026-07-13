56a1c893b6ffaac997e7f2044e4cadefecc0fb26
sanyalankita12 <sanyal.ankita12@gmail.com>
2026-07-13 11:47:38 +0530

    Adding server with Database connection and users table structure.

---
+++ server/server.js
@@ -0,0 +1,13 @@
+require('./config/db');
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server started');
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
