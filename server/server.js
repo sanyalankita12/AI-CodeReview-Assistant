1668626533179ed1afb08ece6e53f13c959f9bdd
sanyalankita12 <sanyal.ankita12@gmail.com>
2026-07-18 18:24:10 +0530

    Prepare backend for Render deployment

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/reviews', reviewRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
